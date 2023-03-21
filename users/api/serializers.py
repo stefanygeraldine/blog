9e627e4d4261ad2d5ba4bf22e718ae3387c1ef65
stefanygeraldine <stefanygeraldine@gmail.com>
2023-03-20 22:20:41 -0300

    password encrypt

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -6,3 +6,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'username', 'password']
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
